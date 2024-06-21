a37d4c4f0390ec190fcb54b7dadb889589d38c53
MARVIN <o.silverygold@gmail.com>
2024-06-21 22:52:15 +0300

    update article router

--- index.mjs
+++ index.mjs
@@ -1,20 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
-import indexRoute from './routes/index.mjs';
-import usersRoute from './routes/users.mjs';
-
+import articleRoute from "./routes/article.mjs";
 
 dotenv.config();
 const PORT = process.env.PORT || 4000;
 const app = express();
 
-app.use('/', indexRoute);
-app.use('/users', usersRoute);
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  const articles = [
+    { title: "Article 1", createdAt: new Date(), desc: "First article" },
+    { title: "Article 2", createdAt: new Date(), desc: "Article two" },
+  ];
+  res.render("articles/index", { articles: articles });
+});
+app.use("/articles", articleRoute);
 
 app.listen(PORT, () => {
   console.log(`Running on port ${PORT}`);
 });
-
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
